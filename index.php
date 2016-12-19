47a61db4bdec6f7d6fed459dce78527cb2cf1828
Mateo Garcia <mateog@stanford.edu>
2016-12-18 21:42:47 -0800

    added custom tip functionality

--- index.php
+++ index.php
@@ -3,21 +3,52 @@
   <head>
     <title>Tip Calculator</title>
     <link type='text/css' rel='stylesheet' href='style.css'/>
+    <?php
+      define('PERCENTAGES', array(10, 15, 20));
+      $defaultPercentage = 15;
+      define('CUSTOM_PERCENTAGE', 'CUSTOM_PERCENTAGE');
+      function hasValidPercentage() {
+        return isset($_POST['percentage']) and in_array((int)$_POST['percentage'], PERCENTAGES);
+      }
+      function hasCustomPercentage() {
+        return isset($_POST['percentage']) and htmlspecialchars($_POST['percentage']) == CUSTOM_PERCENTAGE
+            and isset($_POST['custom-percentage'])
+            and (int)$_POST['custom-percentage'] >= 0;
+      }
+    ?>
     <style type="text/css">
       <?php
-        if (isset($_POST['subtotal'])) {
-          if ((int)$_POST['subtotal'] <= 0) {
-            print 'p.subtotal {
-                color: #A52A2A;
-                font-weight: bold;
-              }
-              input.subtotal {
-                border: 1px solid #A52A2A;
-              }';
-          } else {
+        $shouldShow = true;
+        function showTotal($show) {
+          if ($show) {
             print '.output {
                 display: block;
               }';
+          }
+        }
+        if (isset($_POST['subtotal'])) {
+          if ((int)$_POST['subtotal'] <= 0) {
+            $shouldShow = false;
+            print 'p.subtotal {
+                  color: #A52A2A;
+                  font-weight: bold;
+                }
+                input.subtotal {
+                  border: 1px solid #A52A2A;
+                }';
+          } else {
+            showTotal($shouldShow);
+          }
+        }
+        if (isset($_POST['percentage'])) {
+          if (!hasValidPercentage() and !hasCustomPercentage()) {
+            $shouldShow = false;
+            print 'p.tip-percentage {
+                  color: #A52A2A;
+                  font-weight: bold;
+                };';
+          } else {
+            showTotal($shouldShow);
           }
         }
       ?>
@@ -34,16 +65,17 @@
             }
           ?>
         /></p>
-      <p>Tip percentage:</p>
-      <div class="percentages">
+      <p class="tip-percentage">Tip percentage:</p>
+      <div>
         <ul>
           <?php
-            $percentages = array(10, 15, 20);
-            $checkedValue = 15;
-            if (isset($_POST['percentage']) and in_array((int)$_POST['percentage'], $percentages)) {
+            $checkedValue = $defaultPercentage;
+            if (hasValidPercentage()) {
               $checkedValue = (int)$_POST['percentage'];
+            } else if (hasCustomPercentage()) {
+              $checkedValue = CUSTOM_PERCENTAGE;
             }
-            foreach ($percentages as $value) {
+            foreach (PERCENTAGES as $value) {
               print '<li><input type="radio" name="percentage" value="' . $value . '"';
               if ($value == $checkedValue) {
                 print ' checked="checked"';
@@ -52,6 +84,17 @@
             }
           ?>
         </ul>
+        <p class="custom-percentage">
+          <input type="radio" name="percentage" <?php
+            print 'value="CUSTOM_PERCENTAGE"';
+            if (hasCustomPercentage()) {
+              print ' checked="checked"';
+            }
+          ?>>Custom: <input type="text" name="custom-percentage" size="10"<?php
+            if (hasCustomPercentage()) {
+              print ' value="' . (int)$_POST['custom-percentage'] . '"';
+            }
+          ?>>%</p>
       </div>
       <div class="center">
         <p><input type="submit" /></p>
@@ -61,8 +104,10 @@
       <p>Tip: $<?php
           $subtotal = (int)$_POST['subtotal'];
           $percentage = round(15 / 100, 2);
-          if (isset($_POST['percentage']) and in_array((int)$_POST['percentage'], $percentages)) {
+          if (hasValidPercentage()) {
             $percentage = round((int)$_POST['percentage'] / 100, 2);
+          } else { // custom percentage
+            $percentage = round((int)$_POST['custom-percentage'] / 100, 2);
           }
           $tip = round($subtotal * $percentage, 2);
           print money_format('%.2n', $tip);
